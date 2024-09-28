4c2d678f4203810b799d41be1f73d8ca8f471872
Yang-012 <joy990040eu@gmail.com>
2024-09-28 14:59:14 +0800

    add  json

--- JS/index.js
+++ JS/index.js
@@ -20,7 +20,6 @@ const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector(".prev");
 const todayBtn = document.querySelector(".today");
 const month = document.querySelector(".calendar__monthTitle");
-
 const months = [
   "January",
   "February",
@@ -116,5 +115,61 @@ function hideTodayBtn() {
     todayBtn.style.display = "flex";
   }
 }
+
 renderCalendar();
 //calendar end
+
+//calendar event display start
+const eventDays = document.querySelectorAll(".calendar__day");
+const hotEvents = document.querySelector(".hotEvent__event");
+
+eventDays.forEach((eventDay) => {
+  eventDay.addEventListener("mouseover", () => {
+    const day = eventDay.textContent;
+    const currentDate = new Date();
+    currentDate.setDate(day); // 設置當前日期為滑鼠移入的日期
+
+    const dayOfWeek = currentDate.getDay(); // 取得星期幾 (0 是星期日, 6 是星期六)
+    let hotEvent;
+
+    if (dayOfWeek === 6 || dayOfWeek === 0) {
+      // 如果是星期六或星期日
+      hotEvent = `
+        <h1 class="event__day">週末特別活動</h1>
+        <div class="event__seperator"></div>
+        <h2 class="event__title">水上樂園</h2>
+        <p class="event__time"><i class="fas fa-clock"></i>活動時間: 10:00-16:00</p>
+        <p class="event__location"><i class="fas fa-location-arrow"></i>地點: 水上樂園區</p>
+        <p class="event__others">*歡迎全家參加</p>
+      `;
+    } else {
+      // 如果是平日
+      hotEvent = `
+        <h1 class="event__day">${day}日活動</h1>
+        <div class="event__seperator"></div>
+        <h2 class="event__title">獅子跳火圈</h2>
+        <p class="event__time"><i class="fas fa-clock"></i>第一場次: 09:00-11:00</p>
+        <p class="event__time"><i class="fas fa-clock"></i>第二場次: 13:00-15:00</p>
+        <p class="event__location"><i class="fas fa-location-arrow"></i>地點: 非洲動物園區</p>
+        <p class="event__others">*盡請把握時間觀賞</p>
+      `;
+    }
+
+    hotEvents.innerHTML = hotEvent; // 更新熱點活動資訊
+  });
+
+  eventDay.addEventListener("mouseout", () => {
+    // 當滑鼠移出時恢復預設的活動資訊
+    hotEvents.innerHTML = `
+      <h1 class="event__day">今日活動</h1>
+      <div class="event__seperator"></div>
+      <h2 class="event__title">獅子跳火圈</h2>
+      <p class="event__time"><i class="fas fa-clock"></i>第一場次: 09:00-11:00</p>
+      <p class="event__time"><i class="fas fa-clock"></i>第二場次: 13:00-15:00</p>
+      <p class="event__location"><i class="fas fa-location-arrow"></i>地點: 非洲動物園區</p>
+      <p class="event__others">*盡請把握時間觀賞</p>
+    `;
+  });
+});
+
+//calendar event display end
